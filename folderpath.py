748362894d1f1ba326a016d2c227bf04ca557d0e
Lalitha <lrp29@duke.edu>
2026-06-10 20:30:32 -0400

    added basic file sorting

--- folderpath.py
+++ folderpath.py
@@ -1,2 +1,49 @@
+from pathlib import Path
+import shutil
+import os
 def folderpath(path):
-    print(path)
+    #takes folderpath as input and changes it to the correct format for python code
+    pathy=Path(path)
+    return(pathy)
+def curpath():
+    print(Path.cwd())
+
+def changeroot(path):
+    current = Path.cwd()
+    os.chdir(path)
+def changehome():
+    home = Path.home()
+    os.chdir(home)
+    print(f"current directory is at the home directory{home}")
+def incorrectpath(path):
+    return path.is_dir()
+def docsort(name,p):
+    if(name.suffix == ".txt" or name.suffix == ".docx" or name.suffix == ".pdf"):
+        try:
+            os.mkdir(p/"Docs")
+            os.chdir(p)
+            shutil.move(name,'Docs')
+            
+        except FileExistsError:
+            shutil.move(name,p/'Docs')
+        except PermissionError:
+            shutil.move(p,'Docs')
+def picsort(name,p):
+    if(name.suffix == ".jpg" or name.suffix == ".png" or name.suffix == ".jpeg"):
+        try:
+            os.mkdir(p/"Pics")
+            os.chdir(p)
+            shutil.move(name,'Pics')
+            
+        except FileExistsError:
+            shutil.move(name,p/'Pics')
+        except PermissionError:
+            shutil.move(p,'Pics')
+
+def checkfiles(path):
+    p=Path(path)
+    for name in p.glob('*'):
+        docsort(name,p)
+        picsort(name)
+
+                
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from folderpath import folderpath as fp
+from folderpath import *
 if __name__ == "__main__":
+    curpath()
+    file = input("Type the path of the file here: ")
+    fpath = folderpath(file)
     print("hello. file manager here")
+    print(fpath)
+    checkfiles(fpath)
+    
